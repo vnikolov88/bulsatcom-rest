cf744dbdc3b71db9b1b86769c91e44c806765625
vasil <vastril4o@gmail.com>
2017-12-01 18:39:19 +0200

    INIT > api views

---
+++ onepoint/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace onepoint.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("[controller]/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("[controller]/register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet("[controller]/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
