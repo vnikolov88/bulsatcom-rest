f9fbc7e178f1d38a9ef88e066e0ce0a11e620897
vasil <vastril4o@gmail.com>
2017-12-01 14:26:12 +0200

    FIX > logos are in zip now, all pngs removed from local folder

---
+++ onepoint/Controllers/LogosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace onepoint.Controllers
+{
+    public class LogosController : Controller
+    {
+        [HttpGet("/logos.zip")]
+        public IActionResult GetZip()
+        {
+            FileStream zip = System.IO.File.OpenRead(@".\Resources\data\logos.zip");
+
+            if (zip != null)
+            {
+                return File(zip, "application/zip");
+            }
+
+            return NotFound();
+        }
+    }
+}
